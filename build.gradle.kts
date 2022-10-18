dc80e047256a2853e9dc860d8431e72e4e79d429
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-10-18 08:24:03 +0100

    update for latest plugin
    add a few explanatory comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    id("net.ivoa.vo-dml.vodmltools") version "0.3.9"
+    id("net.ivoa.vo-dml.vodmltools") version "0.3.10"
 }
 
 vodml {
@@ -9,8 +9,8 @@ vodml {
 }
 
 dependencies {
-    implementation("org.javastro.ivoa.vo-dml:ivoa-base:1.0-SNAPSHOT") //IMPL the "standard" base library
-    // Use JUnit Jupiter for testing.
+    implementation("org.javastro.ivoa.vo-dml:ivoa-base:1.0-SNAPSHOT") //reusing the "standard" base library
+    // Use JUnit Jupiter for testing - only necessary if you want to write unit tests!
     implementation("org.slf4j:slf4j-api:1.7.32")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
